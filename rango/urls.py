af8150acd060a07ee22980e8b01ce5d74e2729dc
GZ MIS - Edison Li (李明斌) <edison.li@baleno.network>
2018-04-18 12:16:37 +0800

    Chapter 6 mvt part 2

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.urls import re_path
 from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+            views.show_category, name='show_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'pages': page_list, 'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    return render(request, 'rango/category.html', context_dict)

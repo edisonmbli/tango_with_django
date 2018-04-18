fb825fe817743cbb5246ceb1df9e5c21f68a8909
Edison Li <edisonmbli@gmail.com>
2018-04-18 21:51:29 +0800

    chapter 7 forms part 3

--- rango/urls.py
+++ rango/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add_category/', views.add_category, name='add_category'),
     re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',
             views.show_category, name='show_category'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+            views.add_page, name='add_page'),
 ]

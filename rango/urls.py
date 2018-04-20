250464f50b00a5249b2ee3a0832e1e3ddd4d480a
Edison Li <edisonmbli@gmail.com>
2018-04-21 01:15:54 +0800

    chapter 11 register part 2

--- rango/urls.py
+++ rango/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
             views.show_category, name='show_category'),
     re_path(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
             views.add_page, name='add_page'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    #path('register/', views.register, name='register'),
+    #path('login/', views.user_login, name='login'),
+    #path('logout/', views.user_logout, name='logout'),
     path('restricted/', views.restricted, name='restricted'),
 ]
